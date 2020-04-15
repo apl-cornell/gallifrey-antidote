b10c6f988dd0341fd67326e559a32160454ac1ae
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2020-04-15 17:32:17 -0400

    Updated serialversionUid numbers

--- src/main/java/CRDTEffect.java
+++ src/main/java/CRDTEffect.java
@@ -1,5 +1,5 @@
 public class CRDTEffect extends Effect {
-    private static final long serialVersionUID = 9L;
+    private static final long serialVersionUID = 10L;
     CRDT crdt;
 
     public CRDTEffect(CRDT crdt, VectorClock time) {
--- src/main/java/GenericEffect.java
+++ src/main/java/GenericEffect.java
@@ -1,5 +1,5 @@
 public class GenericEffect extends Effect {
-    private static final long serialVersionUID = 9L;
+    private static final long serialVersionUID = 11L;
     GenericFunction func;
 
     public GenericEffect(GenericFunction func, VectorClock time) {
