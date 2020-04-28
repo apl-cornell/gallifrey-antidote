c8ff61407276e9844e4929a360311073c637b3c5
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2020-04-28 13:49:39 -0400

    More specific object name

--- src/main/java/Snapshot.java
+++ src/main/java/Snapshot.java
@@ -3,11 +3,11 @@ import java.util.TreeSet;
 
 public class Snapshot implements Serializable {
     private static final long serialVersionUID = 12L;
-    public CRDT object;
+    public CRDT crdt;
     public TreeSet<GenericEffect> effectbuffer;
 
-    public Snapshot(CRDT Object, TreeSet<GenericEffect> sortedEffectSet) {
-        this.object = Object;
+    public Snapshot(CRDT crdt, TreeSet<GenericEffect> sortedEffectSet) {
+        this.crdt = crdt;
         this.effectbuffer = sortedEffectSet;
     }
 }
