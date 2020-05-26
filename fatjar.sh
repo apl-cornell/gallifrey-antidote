caaac84314ca49c45df9f4baae2b5a335d679773
Matthew Milano <milano@cs.cornell.edu>
2020-05-26 14:23:22 -0400

    actually put some sanity checking in for the argument

--- fatjar.sh
+++ fatjar.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if echo $1 | egrep -q "\.jar$"; then
+    echo buliding $1
+else
+    echo "Error: required <name>.jar argument"
+    exit 1
+fi
+
 rm -r fatjar || true
 unzip -o 'lib/*' -d fatjar
 unzip -o gallifrey/core/build/libs/core.jar -d fatjar
